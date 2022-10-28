19ffcd2ba1a5e99968b2822d0c4f7eb785e36587
LeonardoMonti <LeonardoMonti>
2022-10-28 19:45:28 -0300

    test: testing individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Product",
+        "Enterprise",
+        "2022-10-28",
+        "2023-10-28",
+        "0001 1111 1222 1333 1444 4",
+        "instrucao",
+    )
+
+    tag_product = (
+        "O produto Product "
+        "fabricado em 2022-10-28 "
+        "por Enterprise com validade "
+        "até 2023-10-28 precisa ser armazenado instrucao."
+    )
+
+    assert str(product) == tag_product
