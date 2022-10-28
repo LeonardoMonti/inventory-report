b73c261df5f883d6e01775ca97814608f5217624
LeonardoMonti <LeonardoMonti>
2022-10-28 19:23:27 -0300

    test: testing product object initializer

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Product",
+        "Enterprise",
+        "2022-10-28",
+        "2023-10-28",
+        "0001 1111 1222 1333 1444 4",
+        "instrucao",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Product"
+    assert product.nome_da_empresa == "Enterprise"
+    assert product.data_de_fabricacao == "2022-10-28"
+    assert product.data_de_validade == "2023-10-28"
+    assert product.numero_de_serie == "0001 1111 1222 1333 1444 4"
+    assert product.instrucoes_de_armazenamento == "instrucao"
